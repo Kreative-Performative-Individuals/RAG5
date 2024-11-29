e6ba009c8d56eede1323a49154b8238979b28a97
MassimilianoBaglioni <3d.massimilianobaglioni@gmail.com>
2024-11-29 02:19:27 +0100

    Added mock post endpoint.

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from typing import List, Optional
+
+class Message(BaseModel):
+    message: str
+
+class BotResponse(BaseModel):
+    response: str
 
 app = FastAPI()
 
 @app.on_event("startup")
 async def startup_event():
-    #Add startup actions.
+    # Startup tasks (if any)
     pass
 
 @app.get("/")
 def root():
     return {"message": "Rag"}
+
+async def calculate_response(user_message: str) -> str:
+    # Call whatever needed to calculate the response.
+    return f"Mocked response to: {user_message}"
+
+@app.post("/chat/", response_model=BotResponse)
+async def chat_with_bot(user_message: Message) -> BotResponse:
+    bot_reply = await calculate_response(user_message.message)
+    return BotResponse(response=bot_reply)
+
