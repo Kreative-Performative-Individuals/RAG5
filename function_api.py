5730fc80aa31762f0e39093e49cf7badc2dac37c
alice <aliceowo@outlook.it>
2024-12-07 19:29:45 +0100

    removed some prints in function_api.py

--- function_api.py
+++ function_api.py
@@ -8,16 +8,12 @@ def ApiRequestCallTopic8(obj):
     data = obj
     response = requests.post(f"{KPI_ENGINE_URL}/kpi/", data=json.dumps(data, default=lambda o: o.__dict__, sort_keys=False, indent=4))
     response = json.loads(response.content)
-    print(response)
     result = response["value"]
     return result
     # assert response.json() == {"response": "Mocked response to: Passed message"}
 
 def ApiRequestCallTopic1(obj):
     request = requests.get(f"{KB_URL}/object-properties", params={"label": obj.name})
-    print(type(request))
-    print("SONO QUI!!!!")
-    print(request.text)
     request = json.loads(request.text)
     obj.name = request["properties"]["label"]
     response = "kpi label: " + request["properties"]["label"] + "\n" + "kpi description: " + request["properties"]["description"]
